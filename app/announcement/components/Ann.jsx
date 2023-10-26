297c2fd0f5f6c71a666538a800084c40fe0ddc08
unknown <privatessg4@gmail.com>
2023-10-26 05:19:43 -0700

    something changed

--- app/announcement/components/Ann.jsx
+++ app/announcement/components/Ann.jsx
@@ -6,6 +6,7 @@ export default function Ann() {
             <h1 className='text-2xl font-bold border-b-2 border-gray-600'>Annoucement</h1>
             <div className="mt-5">
                 {/* <h1>No Any Annoucement</h1> */}
+                
                 <h1>Annoucement Page </h1>
                 <p>THis is Page is next level</p>
             </div>
